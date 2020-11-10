e9cf92181faaaf721aca5dc0a722c6a5291b5277
Kunal Yadwadkar <kunal.y@udacity.com>
2020-11-10 11:33:06 -0800

    Update gitignore.

---
+++ app/graphql/mutations/create_link.rb
@@ -0,0 +1,16 @@
+module Mutations
+  class CreateLink < BaseMutation
+
+    argument :description, String, required: true, description: 'What is this link for?'
+    argument :url, String, required: true, description: 'A unique link'
+
+    type Types::LinkType
+
+
+    def resolve(description: nil, url: nil)
+      Link.create!(description: description,
+                   url: url)
+    end
+
+  end
+end
